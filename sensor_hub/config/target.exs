c60d6d5e94d666f70bb8315b619f2f510c23a71f
J. Bromley <jay@appliedinvention.com>
2022-05-03 12:55:00 -0700

    Add a Nerves-specific SSH key.
    This key allows us to log in from any development machine.

--- sensor_hub/config/target.exs
+++ sensor_hub/config/target.exs
@@ -30,9 +30,8 @@ config :nerves,
 
 keys =
   [
+    Path.join([System.user_home!(), ".ssh", "id_nerves.pub"]),
     Path.join([System.user_home!(), ".ssh", "id_rsa.pub"]),
-    Path.join([System.user_home!(), ".ssh", "id_ecdsa.pub"]),
-    Path.join([System.user_home!(), ".ssh", "id_ed25519.pub"])
   ]
   |> Enum.filter(&File.exists?/1)
 
