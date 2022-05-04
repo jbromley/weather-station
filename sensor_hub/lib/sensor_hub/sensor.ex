b2834879eba8ba6e0e769be3ef6394783524fca4
J. Bromley <jay@appliedinvention.com>
2022-05-03 18:34:06 -0700

    WIP: Wrap SGP40 and BME280 in a standard interface.

---
+++ sensor_hub/lib/sensor_hub/sensor.ex
@@ -0,0 +1,40 @@
+defmodule SensorHub.Sensor do
+  defstruct [:name, :fields, :read, :convert]
+
+  def new(name) do
+    %__MODULE__{
+      read: read_fn(name),
+      convert: convert_fn(name),
+      fields: fields(name),
+      name: name
+    }
+
+    def fields(SGP40), do: [:voc_index]
+    def fields(BMP280), do: [:altitude_m, :pressure_pa, :temperature_c]
+    def fields(VEML7700) do: [:light_lumens]
+
+    # TODO: Wrap the SGP40 to look like an SGP30.
+    def read_fn(SGP40), do: fn -> SGP40.state() end
+    def read_fn(BMP280), do: fn -> BMP280.measure(BMP280) end
+    def read_fn(VEML7700), do: fn -> VEML7700.get_measurement() end
+
+    def convert_fn(SGP40) do
+      fn reading -> Map.take(reading, [:voc_index]) end
+    end
+    def convert_fn(BMP280) do
+      fn reading ->
+        case reading do
+          {:ok, measurement} -> Map.take(measurement, [:altitude_m, :pressure_pa, :temperature_c])
+          _ -> %{}
+        end
+      end
+    end
+    def convert_fn(VEML7700) do
+      fn data -> %{light_lumens: data} end
+    end
+
+    def measure(sensor) do
+      sensor.read.() |> sensor.convert.()
+    end
+
+end
