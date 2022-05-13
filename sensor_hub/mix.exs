c873f68c8c4b3edee5ac20a341c7df2b2c06680d
J. Bromley <jay@appliedinvention.com>
2022-05-13 13:07:44 -0700

    Removed unneeded dependencys from project.
    The sgp30 library is not needed as our project uses the SGP40 sensor and it has
    its own module.

--- sensor_hub/mix.exs
+++ sensor_hub/mix.exs
@@ -42,7 +42,6 @@ defmodule SensorHub.MixProject do
       {:nerves_pack, "~> 0.6.0", targets: @all_targets},
       {:veml7700, path: "../veml7700", targets: @all_targets},
       {:sgp40, "~> 0.1.4", targets: @all_targets},
-      # {:sgp30, "~> 0.2.2", targets: @all_targets},
       {:bmp280, "~> 0.2.5", targets: @all_targets},
 
       # Dependencies for specific targets
